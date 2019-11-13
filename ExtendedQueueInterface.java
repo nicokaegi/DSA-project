d0b334cc8459fe6c52f3e41752430f43963fdc5e
nicokaegi <43972330+nicokaegi@users.noreply.github.com>
2019-11-13 12:59:48 -0500

    Delete ExtendedQueueInterface.java

--- ExtendedQueueInterface.java
+++
@@ -1,6 +0,0 @@
-public interface ExtendedQueueInterface<T> extends QueueInterface<T>{
-  public void enqueueFirst(T newItem) throws ExtendedQueueException;
-  public T dequeueLast() throws ExtendedQueueException;
-  public T peekLast() throws ExtendedQueueException;
-}  // end ExtendedQueueInterface
-
