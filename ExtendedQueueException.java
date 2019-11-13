2efb24850d4fd6d974080b6b62e036dbb96a7345
nicokaegi <43972330+nicokaegi@users.noreply.github.com>
2019-11-13 12:59:40 -0500

    Delete ExtendedQueueException.java

--- ExtendedQueueException.java
+++
@@ -1,6 +0,0 @@
-public class ExtendedQueueException extends RuntimeException {
-
-  public ExtendedQueueException(String s) {
-    super(s);
-  }  // end constructor
-}  // end ExtendedQueueException
